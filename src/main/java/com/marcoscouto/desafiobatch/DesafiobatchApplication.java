fb736e5bdcf57da993a17b704b64d72283f7c90a
Marcos Couto <marcosmartinellicouto@gmail.com>
2022-03-16 20:52:30 -0300

    first commit

---
+++ src/main/java/com/marcoscouto/desafiobatch/DesafiobatchApplication.java
@@ -0,0 +1,13 @@
+package com.marcoscouto.desafiobatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiobatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiobatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcoscouto/desafiobatch/DesafiobatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcoscouto.desafiobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiobatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
